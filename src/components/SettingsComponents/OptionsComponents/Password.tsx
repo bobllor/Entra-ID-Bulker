33d58ed1f5cd27141d408c1a9ad81183ac0725b3
bobllor <bobllor@yahoo.com>
2025-12-02 17:13:49 -0600

    added generate password component

--- src/components/SettingsComponents/OptionsComponents/Password.tsx
+++ src/components/SettingsComponents/OptionsComponents/Password.tsx
@@ -1,10 +1,14 @@
-import { JSX } from "react";
+import { JSX, useState } from "react";
 import OptionBase from "./OptionBase";
 import { OptionProps } from "../types";
 import { useSettingsContext } from "../../../context/SettingsContext";
 import SliderButton from "../../ui/SliderButton";
 import { setSetting } from "../functions";
 import SliderRange from "../../ui/SliderRange";
+import Button from "../../ui/Button";
+import { generatePassword } from "../../../pywebviewFunctions";
+import { FaClipboard } from "react-icons/fa";
+import { toastSuccess } from "../../../toastUtils";
 
 const toolTipText: string = "Password settings";
 const PARENT_KEY: string = "password";
@@ -13,7 +17,11 @@ export default function Password(): JSX.Element{
     const { apiSettings, setApiSettings } = useSettingsContext();
 
     const options: Array<OptionProps> = [
-        {label: "Generate password", element: <></>},
+        {
+            label: "Generate password", 
+            element: <GeneratePassword />, 
+            justify: "between"
+        },
         {
             label: "Password length", 
             element: <SliderRange targetKey="length" baseValue={apiSettings.password.length} 
@@ -47,5 +55,37 @@ export default function Password(): JSX.Element{
         <>
             <OptionBase options={options} title="Password" tooltipText={toolTipText} />
         </>
+    )
+}
+
+function GeneratePassword(): JSX.Element{
+    const [password, setPassword] = useState<string>("");
+
+    return (
+        <div className="flex items-center justify-between w-full">
+            <div className={`${password == "" ? "opacity-0" : "opacity-100"} transition-all`}>
+                <div className="bg-white p-3 input-style rounded-2xl w-70 flex items-center
+                justify-between">
+                    <span>
+                        {password}
+                    </span>
+                    <span 
+                    className="p-2 rounded-xl hover:bg-gray-400"
+                    title="Copy to clipboard"
+                    onClick={() => {
+                        navigator.clipboard.writeText(password).then(() => {
+                            toastSuccess("Copied to clipboard")
+                        });
+                    }}>
+                        <FaClipboard size={18}/>
+                    </span>
+                </div>
+            </div>
+            <Button text="Generate" type="button" func={() => {
+                generatePassword().then(pw => {
+                    setPassword(pw);
+                })
+            }}/>
+        </div>
     )
 }
