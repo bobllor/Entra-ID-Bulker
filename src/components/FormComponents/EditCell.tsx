fb6c834a302ec1ba416c29b8207239a5503a10bd
teboc <bobllor@yahoo.com>
2025-12-06 01:49:05 -0600

    refactored old edit cell logic into a new function

--- src/components/FormComponents/EditCell.tsx
+++ src/components/FormComponents/EditCell.tsx
@@ -1,7 +1,6 @@
 import React, { JSX, useEffect, useRef } from "react";
 import { ManualData } from "./manualUtils/types";
-import toast from "react-hot-toast";
-import { toaster, toastError } from "../../toastUtils";
+import { toastError } from "../../toastUtils";
 import { FaTimes, FaCheck } from "react-icons/fa";
 
 const buttonClass: string = "hover:bg-gray-400 p-2 rounded-xl";
@@ -24,69 +23,29 @@ export default function EditCell({id, stringVal, setEditCell, manData, checkEmpt
                 <input className="py-1 px-2 rounded-xl w-35 input-style"
                 spellCheck={false}
                 ref={inputRef}
-                type="text" defaultValue={stringVal} 
+                type="text" 
+                defaultValue={stringVal} 
                 onKeyDown={(e) => {
                     if(e.key == 'Escape') setEditCell('');
 
                     if(e.key == 'Enter'){
                         const inputVal: string = e.currentTarget.value;
-                        // used only for comparisons
-                        const loweredInputVal: string = inputVal.toLowerCase().trim();
-
-                        if(inputVal.trim() == ''){
-                            toastError('Cannot have an empty value for the Name field');
-                            return;
-                        }
                         
                         if(inputVal.trim() == stringVal){
                             setEditCell('');
                         }else{
-                            const filteredObj: ManualData = manData.manualData.filter((obj) => id.includes(obj.id!))[0];
-
-                            // the else condition guarantees that the input is different than the original column value. 
-                            // yes, it is quite confusing... but i don't want to rewrite my function (hindsight 20/20)
-                            const nameVal: string = filteredObj.name!.toLowerCase();
-                            const columnVal: string = filteredObj.opco!.toLowerCase();
-
-                            if(nameVal == loweredInputVal || columnVal == loweredInputVal){
-                                toaster('Duplicate values in the fields.', "error");
-                                return;
-                            }   
-                            
-                            manData.setManualData(prev => {
-                                const newData: Array<ManualData> = prev.map((obj) => {
-                                    const objID: string = obj.id!;
-                                    
-                                    // id is unique (even with dupes in names)
-                                    // id = obj.id + obj.name, easy way to diff the two columns
-                                    if(id.includes(objID)){
-                                        const columnVal: string = id.replace(objID, '');
-                                        
-                                        for(const key of Object.keys(obj)){
-                                            if(obj[key as keyof ManualData] == columnVal){
-                                                return {
-                                                    ...obj,
-                                                    [key]: inputVal
-                                                }
-                                            }
-                                        }
-                                    }
-                                    
-                                    // i'm like 95% sure this will not be reached.
-                                    return {...obj};
-                                })
-
-                                return newData;
+                            addManualEntry(inputRef, id, manData, checkEmpty).then(status => {
+                                if(status){
+                                    setEditCell("");
+                                }
                             })
-
-                            setEditCell('');
                         };
                     }
                 }}/>
                 <div
                 className="flex gap-1">
                     <span 
-                    onClick={() => addManualEntry(inputRef, checkEmpty).then(
+                    onClick={() => addManualEntry(inputRef, id, manData, checkEmpty).then(
                         (status) => {
                             if(status){
                                 setEditCell('');
@@ -120,20 +79,59 @@ type ManDataProps = {
     setManualData: React.Dispatch<React.SetStateAction<Array<ManualData>>>
 }
 
-async function addManualEntry(inputRef: React.RefObject<HTMLInputElement|null>, checkEmpty: boolean): Promise<boolean>{
+/**
+ * Adds a manual entry to the manual data.
+ * @param inputRef The ref object of the input element.
+ * @param cellId The ID of the current component.
+ * @param manData The manual data state and function object.
+ * @param checkEmpty Boolean to prevent the cell from being empty. If false, then it will accept empty values.
+ * This is also used to determine if the editing cell is the Name field or the Organization (opco) field.
+ * @returns A boolean Promise.
+ */
+async function addManualEntry(inputRef: React.RefObject<HTMLInputElement|null>, cellId: string, manData: ManDataProps, checkEmpty: boolean): Promise<boolean>{
     if(inputRef.current == null){
         return false;
     }
 
-    const value: string = inputRef.current.value;
-    
+    const value: string = inputRef.current.value.trim();
+
     if(value == "" && checkEmpty){
         toastError("Cannot have empty value");
     
         return false;
     }
 
-    console.log(value);
+    // NOTE: the cellId is NOT the same as the id inside the manual data.
+    // according to a previous note by me (june 2025): id = obj.id + obj.name, easy way to diff the two columns
+    // string.includes() must be used. thanks me!
+    const idData: ManualData = {...manData.manualData.filter((man) => (cellId.includes(man.id!)))[0]};
+
+    const checkDuplicate = (data: string, otherData: string) => {
+        if(data == otherData){
+            toastError("Cannot have duplicate values");
+            return false;
+        }
+
+        return true;
+    }
+
+    if(checkEmpty){
+        if(!checkDuplicate(value, idData.opco!)) return false;
+
+        idData.name = value;
+    }else{
+        if(!checkDuplicate(value, idData.name!)) return false;
+
+        idData.opco = value;
+    }
+
+    manData.setManualData(prev => prev.map((ele) => {
+        if(cellId.includes(ele.id!)){
+            return {...idData, id: ele.id};
+        }
+
+        return {...ele};
+    }))
 
     return true;
 }
